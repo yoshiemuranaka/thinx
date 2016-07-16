3e6af274825e4e10fcb2c1a378216d5517c29d9c
Yoshie <yoshiemuranaka@gmail.com>
2016-07-15 23:42:08 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var uglify = require('gulp-uglify');
+
+var paths = {
+  scripts: './js/**/*',
+  scss: './scss/*.scss'
+};
+
+/*
+Task for compiling Sass files
+*/
+gulp.task('build:css', function () {
+  return sass('scss/_styles.scss', {
+  	sourcemap: false, 
+  	noCache: true,
+  	style: 'compressed'
+  })
+  .on('error', sass.logError)
+  .pipe(gulp.dest('styles.css'))
+});
+
+/*
+Task to minify JavaScript files
+*/
+gulp.task('build:js', function() {
+  return gulp.src(paths.scripts)
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch(paths.scss, ['build:css']);
+  gulp.watch(paths.scripts, ['build:js']);
+});
+
+gulp.task('default', ['build:css', 'build:js', 'watch']);
+
